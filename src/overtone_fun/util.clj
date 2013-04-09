65f672a90135f7551a3e6fefd9ed9412adba6c37
Mikkel Gravgaard <mikkel@klokke.dk>
2013-04-09 10:36:22 +0200

    util stuff

--- src/overtone_fun/util.clj
+++ src/overtone_fun/util.clj
@@ -2,6 +2,45 @@
     (:use [overtone.live]
           [overtone.inst.synth]))
 
-(definst beep [note 60]
-  (* (sin-osc (midicps note))
-     (env-gen (perc 0.05 0.15) :action FREE)))
+(defn take-rand [coll n]
+  (take n (shuffle coll) ))
+
+#_(def default-buf (buffer 8))
+#_(buffer-write! default-buf 0 [17 17 17 17])
+
+(int (first (buffer-read def-buf 0 1)))
+
+#_(definst beep [note 60 partials-buffer def-buf]
+  (*
+   (let [partials (buffer-read)
+         f (midicps note)
+         oscs (for [p partials]
+                (sin-osc (* f p)))]
+     (apply + oscs))
+   (env-gen (perc 0.01 0.35) :action FREE)))
+
+#_(beep)
+
+(defn idxs->binvec [idxs]
+  (let [idxs (set (sort idxs))
+        size (+ 1 (last idxs))]
+   (map (fn [i] (if (contains? idxs i) 1 0)) (range size))))
+
+(defn binvec->idxs [b]
+  (->>
+   (map (fn [i] (when (= 1 (nth b i)) i)) (range (count b)))
+   (filter (comp not nil?))))
+
+(defn partials->buf [partials]
+  (let [size (last (sort partials))
+        buf-vec (idxs->binvec partials)
+        buf (buffer (count buf-vec))]
+    (dosync
+     (buffer-write! buf 0 buf-vec)
+     buf)))
+
+
+(defn buf->partials [buf]
+  (let [b (map int (buffer-read buf))
+        b (map )]
+    (filter (comp not nil?) b)))
