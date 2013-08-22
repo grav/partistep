29ee05994d5ef034eb9b12f3c9909169137cbe53
Mikkel Gravgaard <mikkel@klokke.dk>
2013-08-23 00:26:30 +0200

    upped clojure version and (tried to) include core.async

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[overtone "0.8.1"]
-                 [org.clojure/clojure "1.4.0"]])
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]]
+  :repl-options {:port 4555})
