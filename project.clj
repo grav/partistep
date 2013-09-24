dcaddb138911dc630a3edabba5c0467ddd9d7dee
Mikkel Gravgaard <mikkel@klokke.dk>
2013-09-24 23:24:12 +0200

    update to alpha

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[overtone "0.9.0-SNAPSHOT"]
-                                        ;[overtone "0.8.1"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
                  [org.clojure/clojure "1.5.1"]]
   :repl-options {:port 4555})
