bbd130d316ae17bd3868f7779aa2d93daa2d7aba
Mikkel Gravgaard <mikkel@klokke.dk>
2013-09-24 23:25:39 +0200

    move double buffering to atom clean up use core.async for catching midi input

--- src/overtone_fun/launchpad.clj
+++ src/overtone_fun/launchpad.clj
@@ -1,9 +1,8 @@
 (ns overtone-fun.launchpad
   (:require [overtone.midi :as midi])
   (:require [overtone.live :as overtone :refer :all :exclude [buffer]])
-  (:require [clojure.core.async :as a])
+  (:require [clojure.core.async :as a :refer :all])
   (:import [javax.sound.midi ShortMessage]))
-
 
 
 (comment
@@ -17,38 +16,39 @@
 
   )
 
+
 (def launch-out
   ;; seems to be necessary in 0.9.0
   (midi-find-connected-receiver #"Launchpad"))
 
 
-;; monitor stuff
-(on-event
- [:midi :note-on]
- (fn [e]
-   (println (:note e)))
- ::lanchpad-input-handler)
+;; Play around with core.async
+(let [input (chan)]
+  ;; monitor stuff
+  (on-event
+   [:midi :note-on]
+   (fn [e]
+     (go (>! input  e)))
+   ::lanchpad-input-handler)
+
+  (go (while true
+        (when-let [e (<! input)]
+          (println e)))))
+
+
+
+
+
 
 ;; dims of launchpad
-(def w 8)
-(def h 8)
-
-(def tiles (atom (repeat (* w h) 0)))
+(def W 8)
+(def H 8)
 
 (defn tile->midinote
   "Given an index of a square pad,
    return the corresponding midi note."
   [n]
-  (+ n (* 8 (int (/ n 8)))))
-
-(defn show
-  "display tile config on launchpad"
-  [tiles]
-  (doseq [[v n] (map vector tiles (range))]
-    (let [m (tile->midinote n)]
-      (if (> v 0)
-        (midi-note-on launch-out m v)
-        (midi-note-off launch-out m)))))
+  (+ n (* W (int (/ n H)))))
 
 (defn msg [a b c]
   (let [m (ShortMessage.)]
@@ -60,24 +60,46 @@
   (let [m (apply msg bytes)]
     (.send (:receiver launch-out) m -1)))
 
-(defn bar
+(def buff-code {1 0x31
+                0 0x34})
+
+(def buff (atom 1))
+
+(defn other-buffer
+  [current-buffer]
+  (if (= current-buffer 1)
+    0
+    1))
+
+(defn display-buffer
+  [buffer]
+  [0xb0 0x0 (get buff-code buffer)])
+
+(defn show
+  "display tile config on launchpad"
+  [tiles]
+  (swap! buff other-buffer)
+  (send-to-launchpad (display-buffer @buff) )
+  (doseq [[v n] (map vector tiles (range))]
+    (let [m (tile->midinote n)]
+      (if (> v 0)
+        (midi-note-on launch-out m v)
+        (midi-note-off launch-out m))))
+  (send-to-launchpad (display-buffer (other-buffer @buff))))
+
+
+(defn disco
   "Repeatedly display random colors
    on the launchpad. Uses double buffering."
-  ([]
-     (bar true))
-  ([buff1?]
-     (let [t (now)]
-       ;; select certain buffer
-       (send-to-launchpad [0xb0 0x0 (if buff1? 0x31 0x34)])
-       (show (repeatedly (* w h) #(bit-and
-                                   ;; Launchpad Programmer’s Reference pg. 3:
-                                   ;; leaves only color bits intact
-                                   ;; Important regarding dbl buffering
-                                   2r0110011 ;
-                                   (rand-int 128))))
-       (send-to-launchpad [0xb0 0x0 (if buff1? 0x34 0x31)])
-       ;; start again, but with other buffer
-       (apply-at (+ 200 t) #'bar [(not buff1?)]))))
+  []
+  (let [t (now)]
+    (show (repeatedly (* W H) #(bit-and
+                                ;; Launchpad Programmer’s Reference pg. 3:
+                                ;; leaves only color bits intact
+                                ;; Important regarding dbl buffering
+                                2r0110011 ;
+                                (rand-int 128))))
+    (apply-at (+ 200 t) #'disco)))
 
 ;; some useful messages
 (def reset [0xb0 0x0 0x0])
@@ -96,8 +118,8 @@
 (comment
   ;; clear all
   (send-to-launchpad reset)
-  ;; disco
-  (bar)
+
+  (disco)
   ;; no more disco
   (do (stop)
       (send-to-launchpad dblbuff-off)
